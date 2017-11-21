d79073b6ccfe3938e14e42f2f5265a43ea698ad4
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-11-21 12:31:41 +0100

    added feature to add a new feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,19 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
