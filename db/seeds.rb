6abd47706a4a99354d1cdefa1e94e6de17c02335
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-11-21 11:40:08 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(title: "Grocery", details: "Buy pasta and beers")
+Task.create(title: "Laundry", details: "Clothes of this week and the last week")
+Task.create(title: "Cooking", details: "Pasta pesto, Pasta bolo")
+Task.create(title: "Send emails", details: "Emails to Doctolib, Drivy, Center Rome")
+Task.create(title: "Call friends", details: "Call Mercalli, Juste, Vanessa")
