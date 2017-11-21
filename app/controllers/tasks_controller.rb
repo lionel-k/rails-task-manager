f43d0bfccacf3978f81b68d32438e9e32d05f4ae
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-11-21 14:46:56 +0100

    refacto to generate the routes with resources and before_action to acccess a task by an id

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :show, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -17,13 +18,15 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    # raise
-    @task = Task.find(params[:id])
     @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
     redirect_to tasks_path
   end
 
@@ -31,5 +34,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
